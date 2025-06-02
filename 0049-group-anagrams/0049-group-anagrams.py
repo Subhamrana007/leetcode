269c2bc6a66019a1a03b1ef6375a133fbdb839c4
shubham <153279535+Subhamrana007@users.noreply.github.com>
2025-06-02 23:34:33 +0530

    Time: 35 ms (23.67%), Space: 18.1 MB (24.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,16 +4,14 @@ class Solution(object):
         anagram = defaultdict(list)
 
         for s in strs:
-            count = [0] *26
+            count = [0] * 26
 
             for c in s:
                 count[ord(c)-ord("a")] +=1
 
             key = tuple(count)
             anagram[key].append(s)
-        
         return anagram.values()
-
         """
         :type strs: List[str]
         :rtype: List[List[str]]
