57c0922612f0ab70921411eeabdda95f8f051d2f
shubham <153279535+Subhamrana007@users.noreply.github.com>
2025-08-17 01:35:54 +0530

    Time: 0 ms (100%), Space: 12.3 MB (99.03%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.py
+++ 1448-maximum-69-number/1448-maximum-69-number.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def maximum69Number (self, num):
         str_num = str(num)
-        largest_num = int(str_num.replace("6","9",1))
-
-        return (largest_num)
-
+        largest_number = str_num.replace("6","9",1)
+        largest_int = int(largest_number)
+        return largest_int
         """
         :type num: int
         :rtype: int
