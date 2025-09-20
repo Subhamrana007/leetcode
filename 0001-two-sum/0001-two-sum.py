54a69abbea227877c24f313f9fac320cad354d70
shubham <153279535+Subhamrana007@users.noreply.github.com>
2025-09-21 00:37:58 +0530

    Time: 0 ms (100%), Space: 19.1 MB (23.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        empty = {}
-        for i,n in enumerate (nums):
-            diff = target - n
-            if diff in empty:
-                return[ i ,empty[diff]]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in map:
+                return (map[diff], i)
             else:
-                empty[n] = i 
-
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
+                map[num] = i        
