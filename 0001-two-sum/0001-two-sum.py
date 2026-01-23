44837911a9c5cf4bc0ce69b79ef9f56716605b46
Shubham <153279535+Subhamrana007@users.noreply.github.com>
2026-01-23 19:59:51 +0530

    0001-TWO-SUM Stats: Time: 1858 ms (10.37%), Space: 19.8 MB (28.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
-        for i, n in enumerate (nums):
-            diff = target - n
-            if diff in map:
-                return map[diff], i
+        sum = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                sum = nums[i] + nums[j]
+                if sum == target:
+                    return [i, j]
 
-            else:
-                map[n] = i
+            
         
